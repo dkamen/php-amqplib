9ff56bd648e4a0aa289f9d2373da3697783eb70c
dkamen <dkamen@gmail.com>
2017-03-15 15:09:57 +0000

    Update StreamIO.php

--- PhpAmqpLib/Wire/IO/StreamIO.php
+++ PhpAmqpLib/Wire/IO/StreamIO.php
@@ -302,45 +302,46 @@ class StreamIO extends AbstractIO
 
         $this->last_write = microtime(true);
     }
-
+    
+    /* same as write(), but with a much larger buffer (safe in cleartext) */
     public function write_cleartext($data)
     {
         $written = 0;
         $len = mb_strlen($data, 'ASCII');
 
-        
+        while ($written < $len) {
 
-        if (!is_resource($this->sock)) {
-            throw new AMQPRuntimeException('Broken pipe or closed connection');
-        }
-
-        set_error_handler(array($this, 'error_handler'));
-        try {
-            $buffer = fwrite($this->sock, $data);
-        } catch (\ErrorException $e) {
-            restore_error_handler();
-            throw $e;
-        }
-        restore_error_handler();
-
-        if ($buffer === false) {
-            throw new AMQPRuntimeException('Error sending data');
-        }
-
-        if ($buffer === 0 && feof($this->sock)) {
+            if (!is_resource($this->sock)) {
                 throw new AMQPRuntimeException('Broken pipe or closed connection');
+            }
+
+            set_error_handler(array($this, 'error_handler'));
+
+            try {
+                $buffer = fwrite($this->sock, mb_substr($data, $written, 524288, 'ASCII'), 524288);
+            } catch (\ErrorException $e) {
+                restore_error_handler();
+                throw $e;
+            }
+            restore_error_handler();
+
+            if ($buffer === false) {
+                throw new AMQPRuntimeException('Error sending data');
+            }
+
+            if ($buffer === 0 && feof($this->sock)) {
+                throw new AMQPRuntimeException('Broken pipe or closed connection');
+            }
+
+            if ($this->timed_out()) {
+                throw new AMQPTimeoutException('Error sending data. Socket connection timed out');
+            }
+
+            $written += $buffer;
         }
 
-        if ($this->timed_out()) {
-            throw new AMQPTimeoutException('Error sending data. Socket connection timed out');
-        }
-
-        $written += $buffer;
-    
-
-        $this->last_write = microtime(true);  
+        $this->last_write = microtime(true);
     }
-    
     /**
      * Internal error handler to deal with stream and socket errors that need to be ignored
      *
